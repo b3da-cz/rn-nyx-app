2433be289aa2a275f9791bc20e9b13821eb3841e
Tomas Beran <b3da@b3da.net>
2021-04-18 16:50:42 +0200

    Update bundle name

--- android/app/src/main/java/net/b3da/nnn/MainApplication.java
+++ android/app/src/main/java/net/b3da/nnn/MainApplication.java
@@ -1,4 +1,4 @@
-package com.nyx;
+package net.b3da.nnn;
 
 import android.app.Application;
 import android.content.Context;
@@ -62,7 +62,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.nyx.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("net.b3da.nnn.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
--- ios/NnnTests/NnnTests.m
+++ ios/NnnTests/NnnTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface NyxTests : XCTestCase
+@interface NnnTests : XCTestCase
 
 @end
 
-@implementation NyxTests
+@implementation NnnTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
