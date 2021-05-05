9a1c6fb94a1d308a83ea816b557dccd9c61adc22
Tomas Beran <b3da@b3da.net>
2021-05-05 23:29:30 +0200

    Update topic lists with unread toggle

--- src/view/BookmarksView.js
+++ src/view/BookmarksView.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
-import { SectionList, Text } from 'react-native'
+import { SectionList, Text, View } from 'react-native'
+import { FAB } from 'react-native-paper'
 import { DiscussionRowComponent } from '../component'
-import { Context, Styling } from '../lib'
+import { Context, Storage, Styling } from '../lib'
 
 type Props = {
   navigation: any,
@@ -14,6 +15,7 @@ export class BookmarksView extends Component<Props> {
     this.state = {
       reminderCount: 0,
       sectionedBookmarks: [],
+      isShowingRead: false,
       isFetching: false,
     }
     this.navFocusListener = null
@@ -21,6 +23,7 @@ export class BookmarksView extends Component<Props> {
   }
 
   componentDidMount() {
+    this.config = this.context.config
     this.nyx = this.context.nyx
     this.isDarkMode = this.context.theme === 'dark'
     this.navFocusListener = this.props.navigation.addListener('focus', () => {
@@ -32,7 +35,10 @@ export class BookmarksView extends Component<Props> {
         this.getBookmarks()
       }
     })
-    setTimeout(() => this.getBookmarks(), 100)
+    setTimeout(() => {
+      this.init()
+      this.getBookmarks()
+    }, 100)
   }
 
   componentWillUnmount() {
@@ -44,9 +50,13 @@ export class BookmarksView extends Component<Props> {
     }
   }
 
+  init() {
+    this.setState({ isShowingRead: this.config?.isShowingReadOnLists })
+  }
+
   async getBookmarks() {
     this.setState({ isFetching: true })
-    const res = await this.nyx.getBookmarks()
+    const res = await this.nyx.getBookmarks(this.state.isShowingRead)
     if (res?.bookmarks?.length) {
       const reminderCount = res.reminder_count || 0
       const sectionedBookmarks = res.bookmarks.map(b => ({ title: b.category.category_name, data: b.bookmarks }))
@@ -56,42 +66,66 @@ export class BookmarksView extends Component<Props> {
     }
   }
 
+  async toggleRead(isShowingRead) {
+    const config = await Storage.getConfig()
+    config.isShowingReadOnLists = !isShowingRead
+    this.setState({ isShowingRead: !isShowingRead })
+    await Storage.setConfig(config)
+    await this.getBookmarks()
+  }
+
   showDiscussion(id) {
     this.props.onDetailShow(id)
   }
 
   render() {
     return (
-      <SectionList
-        sections={this.state.sectionedBookmarks}
-        stickySectionHeadersEnabled={true}
-        initialNumToRender={100}
-        keyExtractor={(item, index) => item.discussion_id}
-        refreshing={this.state.isFetching}
-        onRefresh={() => this.getBookmarks()}
-        renderSectionHeader={({ section: { title } }) => (
-          <Text
-            style={{
-              fontSize: Styling.metrics.fontSize.medium,
-              color: this.isDarkMode ? Styling.colors.lighter : Styling.colors.darker,
-              backgroundColor: this.isDarkMode ? Styling.colors.dark : Styling.colors.lighter,
-              textAlign: 'right',
-              paddingVertical: 6,
-              paddingHorizontal: Styling.metrics.block.small,
-              marginBottom: Styling.metrics.block.xsmall,
-            }}>
-            {title}
-          </Text>
-        )}
-        renderItem={({ item }) => (
-          <DiscussionRowComponent
-            key={item.discussion_id}
-            discussion={item}
-            isDarkMode={this.isDarkMode}
-            onPress={id => this.showDiscussion(id)}
-          />
-        )}
-      />
+      <View>
+        <SectionList
+          sections={this.state.sectionedBookmarks}
+          stickySectionHeadersEnabled={true}
+          initialNumToRender={100}
+          keyExtractor={(item, index) => item.discussion_id}
+          refreshing={this.state.isFetching}
+          onRefresh={() => this.getBookmarks()}
+          renderSectionHeader={({ section: { title } }) => (
+            <Text
+              style={{
+                fontSize: Styling.metrics.fontSize.medium,
+                color: this.isDarkMode ? Styling.colors.lighter : Styling.colors.darker,
+                backgroundColor: this.isDarkMode ? Styling.colors.dark : Styling.colors.lighter,
+                textAlign: 'left',
+                paddingVertical: 6,
+                paddingHorizontal: Styling.metrics.block.small,
+                marginBottom: Styling.metrics.block.xsmall,
+              }}>
+              {title}
+            </Text>
+          )}
+          renderItem={({ item }) => (
+            <DiscussionRowComponent
+              key={item.discussion_id}
+              discussion={item}
+              isDarkMode={this.isDarkMode}
+              onPress={id => this.showDiscussion(id)}
+            />
+          )}
+        />
+        <FAB
+          small={true}
+          style={{
+            position: 'absolute',
+            margin: 16,
+            right: 0,
+            top: 0,
+            backgroundColor: Styling.colors.darker,
+            opacity: 0.75,
+          }}
+          icon={this.state.isShowingRead ? 'star' : 'star-outline'}
+          visible={true}
+          onPress={() => this.toggleRead(this.state.isShowingRead)}
+        />
+      </View>
     )
   }
 }
--- src/view/HistoryView.js
+++ src/view/HistoryView.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import { ActivityIndicator, ScrollView, RefreshControl, View } from 'react-native'
+import { FAB } from 'react-native-paper'
 import { DiscussionRowComponent } from '../component'
-import { Context, Styling } from '../lib'
+import { Context, Styling, Storage, wait } from '../lib'
 
 type Props = {
   navigation: any,
@@ -13,6 +14,7 @@ export class HistoryView extends Component<Props> {
     super(props)
     this.state = {
       discussions: [],
+      isShowingRead: false,
       isFetching: false,
     }
     this.navFocusListener = null
@@ -32,7 +34,10 @@ export class HistoryView extends Component<Props> {
         this.getHistory()
       }
     })
-    setTimeout(() => this.getHistory(), 100)
+    setTimeout(() => {
+      this.init()
+      this.getHistory()
+    }, 100)
   }
 
   componentWillUnmount() {
@@ -44,10 +49,22 @@ export class HistoryView extends Component<Props> {
     }
   }
 
+  init() {
+    this.setState({ isShowingRead: this.config?.isShowingReadOnLists })
+  }
+
   async getHistory() {
     this.setState({ isFetching: true })
-    const res = await this.nyx.getHistory(this.config?.isShowingReadOnLists)
+    const res = await this.nyx.getHistory(this.state.isShowingRead)
     this.setState({ discussions: res.discussions, isFetching: false })
+  }
+
+  async toggleRead(isShowingRead) {
+    const config = await Storage.getConfig()
+    config.isShowingReadOnLists = !isShowingRead
+    this.setState({ isShowingRead: !isShowingRead })
+    await Storage.setConfig(config)
+    await this.getHistory()
   }
 
   showDiscussion(id) {
@@ -56,36 +73,52 @@ export class HistoryView extends Component<Props> {
 
   render() {
     return (
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={[Styling.groups.themeComponent(this.isDarkMode), { height: '100%' }]}
-        refreshControl={<RefreshControl refreshing={this.state.isFetching} onRefresh={() => this.getHistory()} />}>
-        <View>
-          {this.state.discussions?.length > 0 &&
-            this.state.discussions.map(d => (
-              <DiscussionRowComponent
-                key={d.discussion_id}
-                discussion={d}
-                isDarkMode={this.isDarkMode}
-                onPress={id => this.showDiscussion(id)}
-              />
-            ))}
-          {this.state.discussions && this.state.discussions.length === 0 && (
-            <View
-              style={[
-                {
-                  height: Styling.metrics.window().height - 60,
-                  width: '100%',
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                },
-                Styling.groups.themeComponent(this.isDarkMode),
-              ]}>
-              <ActivityIndicator size="large" color={Styling.colors.primary} style={{ marginBottom: 200 }} />
-            </View>
-          )}
-        </View>
-      </ScrollView>
+      <View style={[Styling.groups.themeComponent(this.isDarkMode), { height: '100%' }]}>
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={[Styling.groups.themeComponent(this.isDarkMode), { height: '100%' }]}
+          refreshControl={<RefreshControl refreshing={this.state.isFetching} onRefresh={() => this.getHistory()} />}>
+          <View>
+            {this.state.discussions?.length > 0 &&
+              this.state.discussions.map(d => (
+                <DiscussionRowComponent
+                  key={d.discussion_id}
+                  discussion={d}
+                  isDarkMode={this.isDarkMode}
+                  onPress={id => this.showDiscussion(id)}
+                />
+              ))}
+            {this.state.discussions && this.state.discussions.length === 0 && (
+              <View
+                style={[
+                  {
+                    height: Styling.metrics.window().height - 60,
+                    width: '100%',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                  },
+                  Styling.groups.themeComponent(this.isDarkMode),
+                ]}>
+                <ActivityIndicator size="large" color={Styling.colors.primary} style={{ marginBottom: 200 }} />
+              </View>
+            )}
+          </View>
+        </ScrollView>
+        <FAB
+          small={true}
+          style={{
+            position: 'absolute',
+            margin: 16,
+            right: 0,
+            top: 0,
+            backgroundColor: Styling.colors.darker,
+            opacity: 0.75,
+          }}
+          icon={this.state.isShowingRead ? 'star' : 'star-outline'}
+          visible={true}
+          onPress={() => this.toggleRead(this.state.isShowingRead)}
+        />
+      </View>
     )
   }
 }
